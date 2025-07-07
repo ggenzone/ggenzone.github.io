a260ff4ebe7a46a769e6372c454900ce2aaa5d58
Guido Genzone <ggenzone@protonmail.com>
2025-07-07 12:00:48 +0200

    feat(root): Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ggenzone.github.io',
   base: 'bitacora',
   vite: {
       plugins: [tailwindcss()]
-	},
+    },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
